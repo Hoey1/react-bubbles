e58047b04b46a1907d1b1470e04545a79907d0b1
Joey <joey@xm8.ninja>
2020-06-19 12:09:27 -0400

    axiosWithAuth added

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouteMatch } from "react-router-dom";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,6 +9,19 @@ import ColorList from "./ColorList";
 const BubblePage = () => {
   const match = useRouteMatch("/bubbles-page");
   const [colorList, setColorList] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("http://localhost:5000/api/colors")
+      .then((res) => {
+        console.log(res.data);
+        setColorList(res.data);
+      })
+      .catch((err) => {
+        console.log("sorry, you are color blind", err);
+      });
+  }, []);
+
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
