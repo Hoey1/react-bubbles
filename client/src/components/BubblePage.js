55d3eb0cd818ab91307893b12797cd12a35ef0ab
Joey <joey@xm8.ninja>
2020-06-19 12:04:37 -0400

    PrivateRoute built Auth functioning

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useRouteMatch } from "react-router-dom";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
+  const match = useRouteMatch("/bubbles-page");
   const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
-
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
@@ -16,5 +15,4 @@ const BubblePage = () => {
     </>
   );
 };
-
 export default BubblePage;
